e0fd6f5bf1f67ef7add58176971afa53bd2086bc
levitrevisan <levitrevisan@hotmail.com>
2019-06-28 17:24:08 -0300

    first commit

---
+++ Objects/main.d
@@ -0,0 +1,15 @@
+.\objects\main.o: main.c
+.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h
+.\objects\main.o: .\RTE\_Target_1\RTE_Components.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\core_cm3.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_version.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h
+.\objects\main.o: .\RTE\Device\STM32F103C8\stm32f10x_conf.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_tim.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\misc.h
---
+++ main.c
@@ -0,0 +1,171 @@
+#include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+
+#define PWM_TIMERS_PRESCALER 0  
+#define PWM_TIMERS_PERIOD 7900
+#define PWM_TIMERS_INIT_PULSE 500
+
+
+void delay(unsigned int nCount);
+void InitializeTimer2();
+void InitializeTimer3();
+void InitializePWMChannels();
+void InitializePWMGPIO();
+void InitializeLEDGPIO();
+void changeRpositiveDutyCycle(int dutyCycle);
+void changeRnegativeDutyCycle(int dutyCycle);
+void changeSpositiveDutyCycle(int dutyCycle);
+void changeSnegativeDutyCycle(int dutyCycle);
+void changeTpositiveDutyCycle(int dutyCycle);
+void changeTnegativeDutyCycle(int dutyCycle);
+
+void setup(){
+	InitializeLEDGPIO();
+	InitializeTimer2();
+	InitializeTimer3();
+	InitializePWMChannels();
+	InitializePWMGPIO();
+}
+
+
+int main(){
+	
+	setup();
+	
+	changeRpositiveDutyCycle(1024);
+	changeRnegativeDutyCycle(4095);
+	changeSpositiveDutyCycle(1024);
+	changeSnegativeDutyCycle(4095);
+	changeTpositiveDutyCycle(1024);
+	changeTnegativeDutyCycle(4095);	
+	
+	while(1){
+	
+		GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+		delay(1000);
+		GPIO_SetBits(GPIOC, GPIO_Pin_13);
+		delay(1000);
+	}
+
+	
+return 0;
+}
+
+void InitializeLEDGPIO(){
+	// Enable clock for GPIOC
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_StructInit(&GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin	= GPIO_Pin_13; //Analog voltages to be measured are connected to pins 1 and 2 of port A, 
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_Mode	= GPIO_Mode_Out_PP; //Analog input mode for ADC
+	GPIO_Init(GPIOC, &GPIO_InitStructure); // calls the function to actually configure the port A.
+}
+
+void InitializePWMGPIO(){
+	// Enable clock for GPIOA
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_StructInit(&GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;					//PA0
+	GPIO_Init(GPIOA , &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;					//PA1
+	GPIO_Init(GPIOA , &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;					//PA2
+	GPIO_Init(GPIOA , &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;					//PA3
+	GPIO_Init(GPIOA , &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;					//PA6
+	GPIO_Init(GPIOA , &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;					//PA7
+	GPIO_Init(GPIOA , &GPIO_InitStructure);
+	
+}
+
+void InitializeTimer2(){
+	// Enable clock for timer 2
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2 , ENABLE);
+	// Create and initialize TimeBase structure
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
+	// Configure TimeBase structure
+	TIM_TimeBaseStructure.TIM_Prescaler	= PWM_TIMERS_PRESCALER;//SystemCoreClock/8000 - 1; // considering 8MHz, 1ms
+	TIM_TimeBaseStructure.TIM_Period = PWM_TIMERS_PERIOD; // 0..999
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+	TIM_Cmd(TIM2, ENABLE);
+	
+}
+
+void InitializeTimer3(){
+	// Enable clock for timer 2
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3 , ENABLE);
+	// Create and initialize TimeBase structure
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
+	// Configure TimeBase structure
+	TIM_TimeBaseStructure.TIM_Prescaler	= PWM_TIMERS_PRESCALER;//SystemCoreClock/8 - 1; // considering 8MHz, 1ms
+	TIM_TimeBaseStructure.TIM_Period = PWM_TIMERS_PERIOD; // 0..999
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+	TIM_Cmd(TIM3, ENABLE);
+	
+}
+
+void InitializePWMChannels(){
+	
+	TIM_OCInitTypeDef TIM_OCInitStructure;
+	TIM_OCStructInit(&TIM_OCInitStructure);
+	
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = PWM_TIMERS_INIT_PULSE;
+	
+	//TIM_OCx < X is the PWM channel number
+	TIM_OC1Init(TIM2, &TIM_OCInitStructure); //PA0
+	TIM_OC2Init(TIM2, &TIM_OCInitStructure); //PA1
+	TIM_OC3Init(TIM2, &TIM_OCInitStructure); //PA2
+	TIM_OC4Init(TIM2, &TIM_OCInitStructure); //PA3
+	TIM_OC1Init(TIM3, &TIM_OCInitStructure); //PA6
+	TIM_OC2Init(TIM3, &TIM_OCInitStructure); //PA7
+
+}
+
+void delay(unsigned int nCount){
+		unsigned int i, j;
+ 
+		for (i = 0; i < nCount; i++)
+				for (j = 0; j < 0x2AFF; j++);
+}
+
+void changeRpositiveDutyCycle(int dutyCycle){
+TIM2->CCR1 = dutyCycle; //0 .. 8191
+}
+
+void changeRnegativeDutyCycle(int dutyCycle){
+TIM2->CCR2 = dutyCycle; //0 .. 8191
+}
+
+void changeSpositiveDutyCycle(int dutyCycle){
+TIM2->CCR3 = dutyCycle; //0 .. 8191
+}
+
+void changeSnegativeDutyCycle(int dutyCycle){
+TIM2->CCR4 = dutyCycle; //0 .. 8191
+}
+
+void changeTpositiveDutyCycle(int dutyCycle){
+TIM3->CCR1 = dutyCycle; //0 .. 8191
+}
+
+void changeTnegativeDutyCycle(int dutyCycle){
+TIM3->CCR2 = dutyCycle; //0 .. 8191
+}
+
+
